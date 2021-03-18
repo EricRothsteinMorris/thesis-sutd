2dda4855237d8f25e5286c3bb174ba869b15c1c8
EricRothsteinMorris <eric_rothstein@sutd.edu.sg>
2021-03-18 11:46:47 +0800

    Changed the word from mutation to transformation because I think it is neater

--- code/Unillanos.hs
+++ code/Unillanos.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import qualified Data.Set as S
 --Definiciones puras de f, g y h
 --f :: Integer -> Integer
 --g :: Integer -> Integer
@@ -34,3 +35,24 @@ reg f x = (show x, f x)
 
 --Nuevo programa
 program' = (reg f) $$$ (reg g) $$$ (reg h)
+
+linearCommon [] _ = []
+linearCommon _ [] = []
+linearCommon xs@(x:xs') ys@(y:ys')
+    | x==y = x:(linearCommon xs' ys')
+    | x<y  = linearCommon xs' ys
+    | x>y  = linearCommon xs  ys'
+
+usingSet::(Ord x)=>[x]->[x]->S.Set x
+usingSet xs ys =
+    S.intersection (S.fromList xs) (S.fromList ys)
+
+usingFoldr xs ys = foldr (\y acc -> if elem y xs then y:acc else acc) [] ys
+-- data Zippable x = Zippable [x] [x]
+
+-- instance Foldable (Zippable) where
+--     foldMap f (Zippable xs ys) =
+--         case (xs,ys) of
+--             ([],_) -> mempty
+--             (_,[]) -> mempty
+--             (x:xs',y:ys') -> --f 
