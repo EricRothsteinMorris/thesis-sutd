67c4f625460d406cd99cfc0161d7b0e98bc53576
EricRothsteinMorris <eric_rothstein@sutd.edu.sg>
2021-03-29 02:51:34 +0800

    I had some fun, but now I have to go to bed so that I can work tomorrow

--- code/HyperlatencySuperSimplified.hs
+++ code/HyperlatencySuperSimplified.hs
@@ -82,33 +82,62 @@ solve seed target =
                solveHelper xs' (fx:fx') solutions' (i+1)
 
 --solveStateDeformation::FCoalgX->FCoalgX->[(M.Map X X)]
-solveStateDeformation::(X->F X)->(X->F X)->[M.Map X X]
+solveStateDeformation::(X->F X)->(X->F X)-> Maybe (M.Map X X)
 solveStateDeformation start current = 
-  solveHelper allTX []
+  solveHelper allTX 
     where 
-      allTX = allPermutations allX --(allTransformations allX)
-      solveHelper xs solutions = 
+      allTX = allTransformations allX--allPermutations allX --(allTransformations allX)
+      solveHelper xs= 
         case xs of
-          [] -> solutions --brute force finished....
+          [] -> Nothing --brute force finished....
           (x:xs') -> --test x with fx
             let 
-              candidate = manifest start (x!) id
-              solutions' = 
-                  if (toFCoalg candidate)==(toFCoalg current) then
-                    --trace ("Found solution: "++(show (x,fx))) $ 
-                    solutions++[x]
-                else
-                    solutions
+              candidate = manifest start (x!) id                
             in
-               solveHelper xs' solutions' 
+                if (toFCoalg candidate)==(toFCoalg current) then
+                    --trace ("Found solution: "++(show (x,fx))) $ 
+                    Just x
+                else
+                    solveHelper xs'
+               
+
+solveBehaviourDeformation::(X->F X)->(X->F X)->Maybe (M.Map (F X) (F X))
+solveBehaviourDeformation start current = 
+  solveHelper allTFX
+    where 
+      allTFX = allTransformations allFX --allPermutations allFX --()
+      solveHelper fxs = 
+        case fxs of
+          [] -> Nothing --brute force finished....
+          (fx:fxs') -> --test x with fx
+            let 
+              candidate = manifest start id (fx!)
+            in
+                if (toFCoalg candidate)==(toFCoalg current) then
+                    --trace ("Found solution: "++(show (x,fx))) $ 
+                    Just fx
+                else
+                    solveHelper fxs' 
 
 {-Ok, so, the idea is the following: I have an original system c, and I am going to choose two transformations: s and t. I write a program for the function t.c.s such that the composition is indistinguishable (this is important, or the other party can reverse c). So, the other party computes (t.c.s)(y). The idea is that there are reverse transformations t' and s' such that (t'.t.c.s.s')=c, and t'((t.c.s)(s' x)) = c(x) for all x. Now, this is the basic idea. The key is (s,t), and it can change every time.  
 
 This is weird... what I am learning here is that there are deformations of the behaviour that can counteract the effect of deforming the state. Does this really work for homomorphic encryption? 
 
-In any case, here is something to think about: imagine that the system is originally c:x->F x, and the attacker deforms the state space, yielding the system (c.s):c->F x. The question is that if there is any type of correction that we can apply, say t:F x-> F x such that (t.c.s)=c, in other words, t counteracts s. This feels a lot like resilience. alternatively, we could find s' such that (c.s'.s)=c and balance the actions of the attacker?
+In any case, here is something to think about: imagine that the system is originally c:x->F x, and the attacker deforms the state space, yielding the system (c.s):c->F x. The question is that if there is any type of correction that we can apply, say t:F x-> F x such that (t.c.s)=c, in other words, t counteracts s. This feels a lot like resilience. alternatively, we could find s' such that (c.s'.s)=c and balance the actions of the attacker? However, playing with time seems more exciting, doesn't it?
 
-To solve the problem above, we need to be able to estimate the deformation. See solveStateDeformation. Well, if the state deformation is reversible then it may be possible just to reverse it as well by counteracting with another state deformation.
+Now, some deformations cannot be reversed. For example 
+
+    solveBehaviourDeformation (manifest flipflop (const True) id) flipflop 
+
+cannot be reversed because (const True) is irreversible. There is also no behaviour transformation to reverse the effects. 
+
+The question I have is that if it is possible to use this to hide a computation, so that the only thing you can learn from it is its type. What worries me is that an arbitrary change of behaviour would lead to jumps in the graph. Imagine the classical automaton 2xX^2. if you can arbitrarily change the behaviour of a state, that is equivalent to changing the arrows and the output of the state. Assume for now that the number of states does not change. With 1 state you can only do two meaninful changes of behaviours {False,{False->(),True->()}} and {True,{False->(),True->()}} (actually, there are no more). What annoys me is that there are limits to what we can do, and they are not very clear to me. Imagine what it would be for me to give you a ((t.c.s):2->2x2^2,x_0) and a w=(i1..in) to compute some answer, and you return me an answer (b,x0..xn). Now, the idea is that I gave you an automaton, you computed something, you gave me an answer, but this happened:
+1) I actually have the three functions t,c, and s that hide my secret system c. 
+2) If I was to give w=(i1..in) to c, it would produce an answer (b', x'0..x'n)
+3) there is a way to recover b' from (b,x0..xn). 
+
+It's fun for the weekend and thursday! Leave it be for now.
+
 -}
 
 
